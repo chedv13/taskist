924d8f13b329bda811bd8c4d11d0076b7cbdccae
Dmitry Chekurov <chedv13@gmail.com>
2015-08-31 13:04:38 +0400

    Дописание маенькой команды, которая удаляет посты старше десяти дней.

--- taskist/blog/management/commands/remove_old_posts.py
+++ taskist/blog/management/commands/remove_old_posts.py
@@ -1,9 +1,12 @@
 from django.core.management import BaseCommand
 from taskist.blog.models import Post
+from datetime import datetime, timedelta
 
 
 class Command(BaseCommand):
     help = "Remove old posts (> 10 days)"
 
     def handle(self, *args, **options):
-        print "Post count: %i" % (Post.objects.count())
+        ten_days_ago = datetime.now() - timedelta(days=10)
+
+        Post.objects.filter(created_at__lte=ten_days_ago).delete()
